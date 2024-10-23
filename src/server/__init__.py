0f63b4bcdf301ee69602c60c309adcd2bd1f4cea
Artemiy Dunaev <a.dunaev@g.nsu.ru>
2024-10-23 15:25:04 +0700

    Created server for post request

--- src/server/__init__.py
+++ src/server/__init__.py
@@ -1,6 +1,6 @@
 import http.server as hs
 import urllib.parse
-
+import os
 
 class RequestHandler(hs.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -8,12 +8,32 @@ class RequestHandler(hs.BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write(b'Hello, World!')
+        return
 
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        params = urllib.parse.parse_qs(body.decode('utf-8'))
-        print(params)
+        # Check if the request contains an image (Content-Type header should indicate this)
+        content_type = self.headers.get('Content-Type')
+        print("Request headers:", dict(self.headers))
+
+        if (content_type and 'images/' in content_type):  # This includes images, thus we proceed
+            # Get image data from request body
+            length = int(self.headers['Content-Length'])
+            body = self.rfile.read(length)
+
+            # Save the received image to disk
+            with open('received_image.' + content_type.split('/')[-1], 'wb') as image_file:
+                image_file.write(body)
+
+            print(f"Image saved to {os.getcwd()}/received_image.{content_type.split('/')[-1]}")
+            self.send_response(200)
+        else:
+            self.send_response(400)  # Bad Request, we can't handle it
+            self.end_headers()
+
+        return
+
+
+    def do_PUT(self):
+        print("Request headers:", dict(self.headers))
         self.send_response(200)
-        self.end_headers()
         return
