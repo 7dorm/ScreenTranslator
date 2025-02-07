984f44a423d067fb181f764f0907d993a9a90739
msansdudu <msansdudu@mail.ru>
2025-02-07 10:12:08 +0700

    words merger first version

---
+++ src/tools/words merger/words merger.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+
+def merger(d):
+    # создали таблицу данных df
+    df = pd.DataFrame(data=d)
+    x1=df.columns[0]
+    y1=df.columns[1]
+    x2=df.columns[2]
+    y2=df.columns[3]
+    letter=df.columns[4]
+
+    # Определяем среднюю ширину буквы
+    avg_width = np.mean(df[x2] - df[x1])
+    max_dist_x = avg_width * 0.5
+    # Определяем среднюю высоту буквы
+    avg_height = np.mean(df[y2] - df[y1])
+    max_dist_y= avg_height
+
+    # Разделяем буквы на строки текста
+    df = df.sort_values(by=y1).reset_index(drop=True) # Отсортировали по возрастанию y
+    lines = []
+    current_line = [df.iloc[0]]
+    for i in range(1, len(df)):
+        if df.iloc[i][y1] - df.iloc[i - 1][y1] <= max_dist_y:
+            current_line.append(df.iloc[i])
+        else:
+            lines.append(pd.DataFrame(current_line))
+            current_line = [df.iloc[i]]
+    lines.append(pd.DataFrame(current_line))
+
+    # Составляем слова
+    words = []
+    for line_df in lines:
+        line_df = line_df.sort_values(by=x1).reset_index(drop=True)  # Сортируем по X
+        current_word = line_df.iloc[0][letter]
+
+        for i in range(1, len(line_df)):
+            if line_df.iloc[i][x1] - line_df.iloc[i - 1][x2] <= max_dist_x:
+                current_word += line_df.iloc[i][letter]
+            else:
+                words.append(current_word)
+                current_word = line_df.iloc[i][letter]
+        words.append(current_word)
+    print(words)
+
+def main():
+    d = pd.DataFrame([
+        (10, 10, 30, 50, 'П'),
+        (35, 10, 55, 50, 'Р'),
+        (60, 10, 80, 50, 'И'),
+        (85, 10, 105, 50, 'В'),
+        (110, 10, 130, 50, 'Е'),
+        (135, 10, 155, 50, 'Т'),
+
+        (10, 70, 30, 110, 'К'),
+        (35, 70, 55, 110, 'А'),
+        (60, 70, 80, 110, 'К')
+    ], columns=['x1', 'y1', 'x2', 'y2', 'letter'])
+    merger(d)
+
+if __name__ == "__main__":
+    main()
